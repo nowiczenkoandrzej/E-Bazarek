b39f137ddccfe8b100e60b2d5e1659d652787ed8
Andrzej Nowiczenko <nowiczenko.andrzej@gmail.com>
2023-08-16 22:36:12 +0200

    test

--- feature_login/src/main/java/com/an/e_bazarek/feature_login/presentation/LoginViewModel.kt
+++ feature_login/src/main/java/com/an/e_bazarek/feature_login/presentation/LoginViewModel.kt
@@ -16,6 +16,8 @@ class LoginViewModel @Inject constructor(
     private val _screenState = MutableStateFlow(LoginState())
     val screenState = _screenState.asStateFlow()
 
+    
+
     fun onEvent(event: LoginScreenEvent) {
         when(event) {
             is LoginScreenEvent.TypeEmail -> {
